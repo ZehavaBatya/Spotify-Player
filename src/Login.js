4e226590e9cf9d0117c7c3fb568de96305d69ede
Annaliese B <84485729+ZehavaBatya@users.noreply.github.com>
2022-11-11 23:47:36 -0500

    Add files via upload

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from "react-boostrap"
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=https://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+export default function Login() {
+    return ( <Container className="d-flex justify-content-center align-items-center">
+        <a className="btn btn-success btn-lg" href={AUTH_URL}>
+            Spotify Login
+        </a>
+    </Container> 
+    )
+}
